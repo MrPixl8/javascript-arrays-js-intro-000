a2f94981ef0765dd215ea99b36ad5259da6ccb01
Jake Lovelace <jaglovelace@gmail.com>
2018-05-18 13:02:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,7 +25,7 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element) {
-  array.shift(element)
+  array.slice(element)
 }
 
 function removeElementFromBeginningOfArray(array, element){
